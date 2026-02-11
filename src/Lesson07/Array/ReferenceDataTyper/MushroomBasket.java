b5d026b7fc887c1c030f71de7a7ec1445b8b05f7
LukasBager <lukasemilbager@protonmail.com>
2026-02-11 11:37:01 +0100

    Mushroom exercise in lesson07 done

--- src/Lesson07/Array/ReferenceDataTyper/MushroomBasket.java
+++ src/Lesson07/Array/ReferenceDataTyper/MushroomBasket.java
@@ -9,6 +9,7 @@ public class MushroomBasket {
         basket[1] = new Mushroom("Portobello", "Brun");
         basket[2] = new Mushroom("Østershat", "Grå");
         basket[3] = new Mushroom("Rød fluesvamp", "Rød");
+        basket[4] = new Mushroom("Shiitake", "Brun");
 
 
         // Udskriv alle svampe med toString()
@@ -27,6 +28,7 @@ public class MushroomBasket {
             System.out.println("The third mushroom does not match.");
         }
 
+
         // Eksempel på ikke-match
         Mushroom wrongMushroom = new Mushroom("Champignon", "Brun");
         if (basket[1].equals(wrongMushroom)) {
@@ -34,5 +36,22 @@ public class MushroomBasket {
         } else {
             System.out.println("The third mushroom does not match Champignon (Brun). ");
         }
+
+
+        checkMushroom = new Mushroom("Østershat", "Grå");
+        if (basket[2].equals(checkMushroom)) {
+            System.out.println("Found!");
+        } else {
+            System.out.println("Not found!");
+        }
+
+
+        for (Mushroom mushroom : basket) {
+            if (mushroom.getColor().equals("Brun")) {
+                System.out.println(mushroom);
+            }
+        }
+
+
     }
 }
